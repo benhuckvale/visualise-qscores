73d22f848a70013df85fe8d84ba3439a024e532d
Ben Huckvale <5511911+benhuckvale@users.noreply.github.com>
2024-05-20 17:41:19 +0100

    Add open graph meta data
    populated using URL environment variable at build time.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
-// https://astro.build/config
+// Set site to the URL environment variable. This will be set to the
+// current deploy domain by the netlify build agent running the build job.
 export default defineConfig({
+  site: import.meta.env.URL || 'http://localhost:3000',
   base: '/qscore/',
   integrations: [react()],
 });
